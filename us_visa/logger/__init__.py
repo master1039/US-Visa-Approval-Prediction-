25c210994a61964a233d036c85cce0084dee54f6
master1039 <saurabh.sinha212@gmail.com>
2024-07-20 17:31:41 +0530

    add connection

---
+++ us_visa/logger/__init__.py
@@ -0,0 +1,17 @@
+import logging
+import os
+
+from datetime import datetime
+from  from_root import from_root
+
+LOG_FILE = f"{datetime.now().strftime('%Y-%m-%d_%H:%M:%S')}.log"
+
+log_dir = 'logs'
+
+logs_path = os.path.join(from_root(), 'log_dir', LOG_FILE)
+os.makedirs(log_dir, exist_ok=True)
+
+logging.basicConfig(filename=logs_path, level=logging.DEBUG,
+                    format='[%(asctime)s]  %(name)s - %(levelname)s - %(message)s')
+                                      
+                                      
