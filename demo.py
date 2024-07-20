5b8f1bfe51383530255cc678ac82621cd1772e42
master1039 <saurabh.sinha212@gmail.com>
2024-07-20 18:17:22 +0530

    Exception & Util code added

--- demo.py
+++ demo.py
@@ -1,3 +1,9 @@
 from us_visa.logger import logging
+from us_visa.exception import USVisaException
+import sys
 
-logging.info("Welcome to")
+try:
+    a = 1/"10"
+except Exception as e:
+    logging.info(e)
+    raise USVisaException(e,sys) from e
